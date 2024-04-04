2b0ac5a4ba03c4ae81bbbd22dd4ef7535d65101c
Menghi Gabriele <gabry.menghi01@gmail.com>
2024-04-04 19:47:36 +0200

    Add button to import an excel file (only excel extension accepted)

--- OrderManager/ClientApp/src/app/components/import/import.component.ts
+++ OrderManager/ClientApp/src/app/components/import/import.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrl: './import.component.css'
 })
 export class ImportComponent {
+  onFileSelected(event: any) {
+    const selectedFile = event.target.files[0];
+    const allowedExtensions = ['.xlsx', '.xlsm'];
 
+    // Verifica se l'estensione del file selezionato è tra quelle consentite
+    if (selectedFile) {
+      const fileExtension = selectedFile.name.split('.').pop()?.toLowerCase();
+      if (fileExtension && allowedExtensions.includes('.' + fileExtension)) {
+        // Il file ha un'estensione consentita, puoi procedere con il tuo codice
+      } else {
+        // Il file ha un'estensione non consentita
+        alert("Sono consentiti solo file .xlsx o .xlsm");
+        event.target.value = null;
+      }
+    }
+  }
 }
