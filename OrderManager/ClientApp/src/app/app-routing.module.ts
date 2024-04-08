63eee6dcb8ab83d4e1550a4ce86bf21e0bf1a1ae
Menghi Gabriele <gabry.menghi01@gmail.com>
2024-04-08 21:52:35 +0200

    Add clients page into sidebar

--- OrderManager/ClientApp/src/app/app-routing.module.ts
+++ OrderManager/ClientApp/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProdottiComponent } from './components/prodotti/prodotti.component';
 import { HomeComponent } from './components/home/home.component';
 import { ImportComponent } from './components/import/import.component';
+import { ClientiComponent } from './components/clienti/clienti.component';
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
   {path:'prodotti',component:ProdottiComponent},
   {path:'import',component:ImportComponent},
+  {path:'clienti',component:ClientiComponent},
 ];
 
 @NgModule({
