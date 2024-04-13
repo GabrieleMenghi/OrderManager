66aaee3cb9ad4c349ffadc63fcdd7d7657d89e87
Menghi Gabriele <gabry.menghi01@gmail.com>
2024-04-13 10:40:08 +0200

    Complete products table - filter, order, pagination

--- OrderManager/ClientApp/src/app/components/prodotti/prodotti.component.ts
+++ OrderManager/ClientApp/src/app/components/prodotti/prodotti.component.ts
@@ -1,76 +1,46 @@
-import { Component, OnInit } from '@angular/core';
-import { ConfigService } from '../../config/config.service';
-import { FormBuilder } from '@angular/forms';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { Prodotto } from '../../models/prodotto.model';
+import { ConfigService } from '../../config/config.service';
 
 @Component({
   selector: 'app-prodotti',
-  templateUrl: './prodotti.component.html',
   styleUrl: './prodotti.component.css',
+  templateUrl: './prodotti.component.html',
 })
-export class ProdottiComponent implements OnInit {
+export class ProdottiComponent implements AfterViewInit, OnInit {
+  displayedColumns: string[] = ['codice', 'descrizione', 'prezzo'];
+  dataSource: MatTableDataSource<Prodotto>;
   prodotti: Array<Prodotto> = [];
-  public filteredProdotti: Array<Prodotto> = [];
-  public currentItems: Array<Prodotto> = [];
-  binding: any;
-  public totalItems: number = 0;
-  public pageSize: number = 25;
 
-  constructor(
-    private configService: ConfigService,
-    private formBuilder: FormBuilder
-  ) {}
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
   async ngOnInit() {
     (await this.configService.getProdotti()).subscribe((data) => {
       this.prodotti = data as Array<Prodotto>;
-
-      // Eseguito dentro la callback, altrimenti le assegnazioni sarebbero avvenute prima, ad array vuoti
-      this.filteredProdotti = this.prodotti;
-      this.currentItems = this.filteredProdotti;
-  
-      // Numero totale di elementi
-      this.totalItems = this.filteredProdotti.length;
-      // Chiamata iniziale per impostare correttamente gli elementi della pagina corrente
-      this.onPageChange({pageIndex: 0, pageSize: this.pageSize});
+      this.dataSource.data = this.prodotti;
     });
-    
   }
 
-  public prodottiSearchText: any;
+  constructor(private configService: ConfigService) {
+    // Assign the data to the data source for the table to render
+    this.dataSource = new MatTableDataSource(this.prodotti);
+  }
 
-  searchProdottiForm = this.formBuilder.group({
-    prodottiSearchText: '',
-  });
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
 
-  onTextChange() {
-    if (this.prodottiSearchText) {
-      const searchText = this.prodottiSearchText.toLowerCase(); // Converti il testo di ricerca in minuscolo
-      this.filteredProdotti = this.prodotti.filter(
-        (p) => p.descrizione.toLowerCase().includes(searchText) // Controlla se la descrizione contiene il testo di ricerca
-      );
-      this.currentItems = this.filteredProdotti;
-      this.onPageChange({ pageIndex: 0, pageSize: this.pageSize }); // Riporta alla prima pagina dopo la ricerca
-    } else {
-      // Se il campo di ricerca è vuoto, mostra tutti i prodotti
-      this.filteredProdotti = this.prodotti;
-      this.currentItems = this.filteredProdotti;
-      this.onPageChange({ pageIndex: 0, pageSize: this.pageSize }); // Riporta alla prima pagina dopo la ricerca
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
     }
-    this.totalItems = this.filteredProdotti.length;
-  }
-
-  pageSizeOptions: number[] = [5, 10, 25, 100];
-
-  // Metodo chiamato quando cambia la pagina
-  onPageChange(event: any) {
-    const startIndex = event.pageIndex * event.pageSize;
-    const endIndex = startIndex + event.pageSize;
-    // Estrai gli elementi corrispondenti alla pagina corrente
-    // Se items è un array di oggetti
-    this.currentItems = this.filteredProdotti.slice(startIndex, endIndex);
-    // Se items è un array di stringhe o numeri
-    // this.currentItems = this.items.slice(startIndex, endIndex);
-    this.pageSize = event.pageSize;
   }
 }
