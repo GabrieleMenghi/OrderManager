4a9adf2d398d00c2213e9bb299684a6c4259aa3a
Menghi Gabriele <gabry.menghi01@gmail.com>
2024-04-20 10:05:36 +0200

    Create orders repository

---
+++ OrderManager/Repo/Int/IRepositoryOrdini.cs
@@ -0,0 +1,9 @@
+﻿using OrderManager.DB;
+
+namespace OrderManager.Repo.Int;
+
+public interface IRepositoryOrdini
+{
+    Task<Ordine> AddOrder(Ordine ordine);
+    Task<List<Ordine>> GetOrders();
+}
---
+++ OrderManager/Repo/SqlRepositoryOrdini.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderManager.DB;
+using OrderManager.Repo.Int;
+
+namespace OrderManager.Repo;
+
+public class SqlRepositoryOrdini : IRepositoryOrdini
+{
+    private readonly OrderManagerDBContext _dbContext;
+
+    public SqlRepositoryOrdini(OrderManagerDBContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<List<Ordine>> GetOrders()
+    {
+        return await _dbContext.Ordini
+            .Include(x => x.RigheOrdine)
+            .ToListAsync();
+    }
+
+    public async Task<Ordine> AddOrder(Ordine ordine)
+    {
+        await _dbContext.Ordini.AddAsync(ordine);
+        await _dbContext.SaveChangesAsync();
+
+        return ordine;
+    }
+}
